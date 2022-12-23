f65621dc8677ed5ed6cae19ae2483b8b30ec4a7a
Carter <Carterolson3@gmail.com>
2022-12-23 00:43:07 -0600

    test

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ course }) => {
   const dispatch = useDispatch();
-  //test
+  //test 12
   const handleSelect = () => {
     dispatch(setSelectedFile(course));
   };
